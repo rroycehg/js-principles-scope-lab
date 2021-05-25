339a97dd99326fc2efabc1e66c79a76f49ac24f4
Roli <rroycehg@gmail.com>
2021-05-25 00:21:32 -0400

    commit tree 62bb41eb3317febe7d556e186d150192b5ebc7bf

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = `bob`;
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+    //purposely not using let, const, or var
+    bestCustomer = `not bob`;
+
+}
+
+function overwriteBestCustomer() {
+    //calling bestCustomer from above (?)
+    bestCustomer = `maybe bob`;
+}
+
+const notFavoriteCustomer = `Robert`;
+
+function changeLeastFavoriteCustomer() {
+    notFavoriteCustomer = `bob`;    
+}
